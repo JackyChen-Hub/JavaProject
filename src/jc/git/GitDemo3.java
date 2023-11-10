f0d3b084c3a3792e9be4be4c7f2d32ea65778e47
jacky <jacky@DESKTOP-6JEGS77.ht.home>
2023-11-10 15:41:16 -0500

    committed in Branch-X

--- src/jc/git/GitDemo3.java
+++ src/jc/git/GitDemo3.java
@@ -1,11 +1,11 @@
 package jc.git;
 
-public class GitDemo2 {
+public class GitDemo3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//added comment-2
-		System.out.println("Git demo2");
+		System.out.println("Git demo3");
 
 	}
 
